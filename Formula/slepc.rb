54e4031994fe038dd15dc250dc3c24f4ca1ebd23
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2018-01-27 14:30:20 +0100

    initial formula

---
+++ Formula/slepc.rb
@@ -0,0 +1,87 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+ class Slepc < Formula
+  desc ""
+  homepage ""
+  url "http://slepc.upv.es/download/distrib/slepc-3.8.2.tar.gz"
+  sha256 "1e7d20d20eb26da307d36017461fe4a55f40e947e232739179dbe6412e22ed13"
+
+  bottle :disable, "needs to be rebuilt with latest open-mpi"
+
+  deprecated_option "complex" => "with-complex"
+
+  option "with-complex", "Use complex version by default. Otherwise, real-valued version will be symlinked"
+  option "without-test", "Skip run-time tests (not recommended)"
+  option "with-openblas", "Install dependencies with openblas"
+  option "with-blopex", "Download blopex library"
+
+  deprecated_option "without-check" => "without-test"
+
+  openblasdep = (build.with? "openblas") ? ["with-openblas"] : []
+
+  depends_on "petsc" => openblasdep
+  depends_on "open-mpi" => [:cc, :f90]
+  depends_on "gcc"
+  depends_on "hdf5"
+  depends_on :x11 => :optional
+  depends_on "arpack" => [:recommended, "with-mpi"] + openblasdep
+
+  def install
+    ENV.deparallelize
+
+    # these must be consistent with petsc.rb
+    petsc_arch_real = "real"
+    petsc_arch_complex = "complex"
+
+    ENV["SLEPC_DIR"] = Dir.getwd
+    args = ["--with-clean=true"]
+    args << "--with-arpack-dir=#{Formula["arpack"].opt_lib}" << "--with-arpack-flags=-lparpack,-larpack" if build.with? "arpack"
+    args << "--download-blopex" if build.with? "blopex"
+
+    # real
+    ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/#{petsc_arch_real}"
+    system "./configure", "--prefix=#{prefix}/#{petsc_arch_real}", *args
+    system "make"
+    system "make", "test" if build.with? "test"
+    system "make", "install"
+
+    # complex
+    ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/#{petsc_arch_complex}"
+    system "./configure", "--prefix=#{prefix}/#{petsc_arch_complex}", *args
+    system "make"
+    # TODO: investigate why complex tests fail to run on Linuxbrew
+    system "make", "test" if build.with? "test"
+    system "make", "install"
+
+    # Link what we need.
+    petsc_arch = ((build.include? "complex") ? petsc_arch_complex : petsc_arch_real)
+
+    include.install_symlink Dir["#{prefix}/#{petsc_arch}/include/*.h"],
+                            "#{prefix}/#{petsc_arch}/finclude", "#{prefix}/#{petsc_arch}/slepc-private"
+    lib.install_symlink Dir["#{prefix}/#{petsc_arch}/lib/*.*"]
+    prefix.install_symlink "#{prefix}/#{petsc_arch}/conf"
+    doc.install "docs/slepc.pdf", Dir["docs/*.htm"], "docs/manualpages" # They're not really man pages.
+    pkgshare.install "share/slepc/datafiles"
+
+    # install some tutorials for use in test block
+    pkgshare.install "src/eps/examples/tutorials"
+  end
+
+  def caveats; <<~EOS
+    Set your SLEPC_DIR to #{opt_prefix}/real or #{opt_prefix}/complex.
+    Fortran modules are in #{opt_prefix}/real/include and #{opt_prefix}/complex/include.
+    EOS
+  end
+
+  test do
+    cp_r prefix/"share/slepc/tutorials", testpath
+    Dir.chdir("tutorials") do
+      system "mpicc", "ex1.c", "-I#{opt_include}", "-I#{Formula["petsc"].opt_include}", "-L#{Formula["petsc"].opt_lib}", "-lpetsc", "-L#{opt_lib}", "-lslepc", "-o", "ex1"
+      system "mpirun -np 3 ex1 2>&1 | tee ex1.out"
+      `cat ex1.out | tail -3 | awk '{print $NF}'`.split.each do |val|
+        assert val.to_f < 1.0e-8
+      end
+    end
+  end
+end
---
+++ Formula/superlu_dist.rb
@@ -0,0 +1,60 @@
+class SuperluDist < Formula
+  desc "Distributed LU factorization for large linear systems"
+  homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/"
+  url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_dist_5.1.0.tar.gz"
+  sha256 "30ac554a992441e6041c6fb07772da4fa2fa6b30714279de03573c2cad6e4b60"
+  revision 3
+
+  bottle :disable, "needs to be rebuilt with latest open-mpi"
+
+  option "without-test", "skip build-time tests (not recommended)"
+  option "with-openmp", "Enable OpenMP multithreading"
+  needs :openmp if build.with? "openmp"
+
+  depends_on "cmake" => :build
+  depends_on "gcc"
+  depends_on "open-mpi" => [:cc, :f77, :f90]
+
+  depends_on "parmetis"
+
+  def install
+    # prevent linking errors on linuxbrew:
+    ENV.deparallelize
+    ENV["MPICC"] = "mpicc"
+    ENV["MPICXX"] = "mpicxx"
+    ENV["MPIF90"] = "mpif90"
+
+    dylib_ext = OS.mac? ? "dylib" : "so"
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DTPL_PARMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.#{dylib_ext};#{Formula["metis"].opt_lib}/libmetis.#{dylib_ext}"
+    cmake_args << "-DTPL_PARMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include};#{Formula["metis"].opt_include}"
+    cmake_args << "-DCMAKE_C_FLAGS=-fPIC -O2"
+    cmake_args << "-DBUILD_SHARED_LIBS=ON"
+    cmake_args << "-DCMAKE_C_COMPILER=#{ENV["MPICC"]}"
+    cmake_args << "-DCMAKE_Fortran_COMPILER=#{ENV["MPIF90"]}"
+    cmake_args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+
+    blaslib = ("/System/Library/Frameworks/Accelerate.framework/Accelerate")
+    cmake_args << "-DTPL_BLAS_LIBRARIES=#{blaslib}"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make"
+      system "make", "install"
+      system "make", "test" if build.with? "test"
+    end
+
+    doc.install "DOC/ug.pdf"
+    pkgshare.install "EXAMPLE"
+  end
+
+  test do
+    ENV.fortran
+    cp pkgshare/"EXAMPLE/dcreate_matrix.c", testpath
+    cp pkgshare/"EXAMPLE/pddrive.c", testpath
+    cp pkgshare/"EXAMPLE/g20.rua", testpath
+    system "mpicc", "-o", "pddrive", "pddrive.c", "dcreate_matrix.c", "-L#{Formula["superlu_dist"].opt_lib}", "-lsuperlu_dist"
+    system "mpirun", "-np", "4", "./pddrive", "-r", "2", "-c", "2", "g20.rua"
+  end
+end
