4461af686acc3940713e8a4a1c5b40fa2a9733dc
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2019-05-15 17:02:18 +0200

    add open-mpi version 3.14 to work around open-mpi 4 bugs

---
+++ Formula/open-mpi.rb
@@ -0,0 +1,81 @@
+class OpenMpi < Formula
+  desc "High performance message passing library"
+  homepage "https://www.open-mpi.org/"
+  url "https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.4.tar.bz2"
+  sha256 "17a69e0054db530c7dc119f75bd07d079efa147cf94bf27e590905864fe379d6"
+
+  head do
+    url "https://github.com/open-mpi/ompi.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "gcc"
+  depends_on "libevent"
+
+  conflicts_with "mpich", :because => "both install MPI compiler wrappers"
+
+  def install
+    # otherwise libmpi_usempi_ignore_tkr gets built as a static library
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-ipv6
+      --with-libevent=#{Formula["libevent"].opt_prefix}
+      --with-sge
+    ]
+    args << "--with-platform-optimized" if build.head?
+
+    system "./autogen.pl" if build.head?
+    system "./configure", *args
+    system "make", "all"
+    system "make", "check"
+    system "make", "install"
+
+    # Fortran bindings install stray `.mod` files (Fortran modules) in `lib`
+    # that need to be moved to `include`.
+    include.install Dir["#{lib}/*.mod"]
+  end
+
+  test do
+    (testpath/"hello.c").write <<~EOS
+      #include <mpi.h>
+      #include <stdio.h>
+      int main()
+      {
+        int size, rank, nameLen;
+        char name[MPI_MAX_PROCESSOR_NAME];
+        MPI_Init(NULL, NULL);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Get_processor_name(name, &nameLen);
+        printf("[%d/%d] Hello, world! My name is %s.\\n", rank, size, name);
+        MPI_Finalize();
+        return 0;
+      }
+    EOS
+    system bin/"mpicc", "hello.c", "-o", "hello"
+    system "./hello"
+    system bin/"mpirun", "./hello"
+    (testpath/"hellof.f90").write <<~EOS
+      program hello
+      include 'mpif.h'
+      integer rank, size, ierror, tag, status(MPI_STATUS_SIZE)
+      call MPI_INIT(ierror)
+      call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
+      call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+      print*, 'node', rank, ': Hello Fortran world'
+      call MPI_FINALIZE(ierror)
+      end
+    EOS
+    system bin/"mpif90", "hellof.f90", "-o", "hellof"
+    system "./hellof"
+    system bin/"mpirun", "./hellof"
+  end
+end
