eb4e1658543945d32c4b77f22af7a5cce28b7552
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-01-29 16:15:31 +0100

    add paraview recipe

---
+++ Formula/paraview.rb
@@ -0,0 +1,64 @@
+ class ParaView < Formula
+  desc ""
+  homepage "https://www.paraview.org/"
+  url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=source&os=Sources&downloadFile=ParaView-v5.10.0.tar.xz"
+  sha256 "86d85fcbec395cdbc8e1301208d7c76d8f48b15dc6b967ffbbaeee31242343a5"
+
+  depends_on "cmake"
+  depends_on "gcc"
+  depends_on "open-mpi"
+  depends_on "ffmpeg"
+  depends_on "python@3.9"
+  depends_on "qt@5"
+  depends_on "hdf5-mpi"
+  depends_on "openblas"
+  depends_on "eigen"
+
+  def install
+    args = std_cmake_args + %W[
+      -DCMAKE_INSTALL_NAME_DIR:STRING=#{opt_lib}
+      -DCMAKE_INSTALL_RPATH:STRING=#{rpath}
+      -DBLAS_LIBRARIES=/usr/local/opt/openblas/lib/libblas.dylib
+      -DLAPACK_LIBRARIES=/usr/local/opt/openblas/lib/liblapack.dylib 
+      -DCMAKE_Fortran_COMPILER:STRING=gfortran 
+      -DQt5_DIR:STRING=/usr/local/opt/qt@5/lib/cmake/Qt5 
+      -DBUILD_TESTING:BOOL=OFF 
+      -DCMAKE_BUILD_TYPE:STRING=Release 
+      -DPARAVIEW_INSTALL_DEVELOPMENT_FILES:BOOL=ON 
+      -DPARAVIEW_USE_MPI:BOOL=ON 
+      -DPython3_EXECUTABLE:FILEPATH=#{Formula["python@3.9"].opt_bin}/python3
+      -DPARAVIEW_ENABLE_XDMF3:BOOL=ON 
+      -DPARAVIEW_ENABLE_FFMPEG:BOOL=ON 
+      -DVTK_FORBID_DOWNLOADS:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_ParaView_protobuf:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_ParaView_cgns:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_utf8:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_doubleconversion:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_lz4:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_pugixml:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_jsoncpp:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_libharu:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_glew:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_ogg:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_theora:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_gl2ps:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_libxml2:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_lzma:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_tiff:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_netcdf:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_expat:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_eigen:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_freetype:BOOL=OFF 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_hdf5:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_mpi4py:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_jpeg:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_png:BOOL=ON 
+      -DVTK_MODULE_USE_EXTERNAL_VTK_zlib:BOOL=ON 
+      -DHDF5_NO_FIND_PACKAGE_CONFIG_FILE:BOOL=ON
+
+      mkdir "build" do
+        system "cmake", "..", *args
+        system "make"
+        system "make", "install"
+      end
+end
