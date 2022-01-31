c70f3d939affba3103281a2175e099ade7aa8175
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-01-31 17:55:27 +0100

    hmm

--- Formula/mytestpack.rb
+++ Formula/mytestpack.rb
@@ -2,7 +2,7 @@
   desc ""
   homepage "https://www.paraview.org/"
   url "https://github.com/marcalexanderschweitzer/homebrew-science/blob/master/Formula/mytestpack-1.0.tar.gz?raw=true"
-  sha256 "f30e414c9b73c7b41c75d0ba05e26772fe567c40cc8423822272fd45263460f4"
+  sha256 "e08f7ebb1318bffe4ec250c7d73591cae279fd6c767e6ad0349274d3186b6aed"
 
   depends_on "cmake"
 
