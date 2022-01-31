e85e35593c9677a874faad3c27a4890ef152c0f2
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-01-31 16:48:18 +0100

    test pack

---
+++ Formula/mytestpack.rb
@@ -0,0 +1,24 @@
+ class Mytestpack < Formula
+  desc ""
+  homepage "https://www.paraview.org/"
+  url "https://github.com/marcalexanderschweitzer/homebrew-science/blob/master/Formula/mytestpack--1.0.0.tar.gz?raw=true"
+  sha256 ""
+
+  depends_on "cmake"
+  depends_on "cereal"
+
+def install
+    args = std_cmake_args + %W[
+      -G Ninja 
+      -DBUILD_TESTING:BOOL=OFF
+      ]
+
+      mkdir "build" do
+        system "cmake", "..", *args
+        system "make"
+        system "make install"
+      end
+
+    #   bin.install_symlink Applications/paraview.app/Contents/MacOS/"paraview"
+  end
+end
