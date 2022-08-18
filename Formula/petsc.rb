237d4164040f3308cf2263ee56fb43bfbeb86439
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-08-18 10:59:35 +0200

    compiler petsc mpi

--- Formula/petsc.rb
+++ Formula/petsc.rb
@@ -50,15 +50,16 @@
   depends_on "suite-sparse"
 
   def install
-    ENV["CC"] = "mpicc"
-    ENV["CXX"] = "mpicxx"
-    ENV["F77"] = "mpif77"
-    ENV["FC"] = "mpif90"
+    # ENV["CC"] = "mpicc"
+    # ENV["CXX"] = "mpicxx"
+    # ENV["F77"] = "mpif77"
+    # ENV["FC"] = "mpif90"
     ENV["PETSC_DIR"] = Dir.getwd
 
     arch_real = "real"
     ENV["PETSC_ARCH"] = arch_real
-    system "./configure", "CC=mpicc", "CXX=mpicxx", "FC=mpif90", "F77=mpif77",
+    system "./configure", #"CC=mpicc", "CXX=mpicxx", "FC=mpif90", "F77=mpif77",
+                          "--with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90",
                           "--with-shared-libraries=1",
                           "--with-pthread=0",
                           "--with-openmp=0",
@@ -90,6 +91,7 @@
     arch_complex = "complex"
     ENV["PETSC_ARCH"] = arch_complex
     system "./configure", "CC=mpicc", "CXX=mpicxx", "FC=mpif90", "F77=mpif77",
+                          "--with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90",
                           "--with-shared-libraries=1",
                           "--with-pthread=0",
                           "--with-openmp=0",
