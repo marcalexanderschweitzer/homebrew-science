20d3a101a1854b976b8e853f536f367cce5f1bec
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-11-07 15:53:59 +0100

    superlu_dist ventura

--- Formula/superlu_dist.rb
+++ Formula/superlu_dist.rb
@@ -12,19 +12,17 @@
   depends_on "marcalexanderschweitzer/science/parmetis"
 
   def install
-
-
     args = %W[
-      -DCMAKE_C_COMPILER="#{Formula["open-mpi"].opt_bin}/mpicc"
-      -DCMAKE_FC_COMPILER="#{Formula["open-mpi"].opt_bin}/mpif90"
-      -DCMAKE_CXX_COMPILER="#{Formula["open-mpi"].opt_bin}/mpicxx"
-      -DCMAKE_CXX_FLAGS="-std=c++11" 
+      -DCMAKE_C_COMPILER=#{Formula["open-mpi"].opt_bin}/mpicc
+      -DCMAKE_FC_COMPILER=#{Formula["open-mpi"].opt_bin}/mpif90
+      -DCMAKE_CXX_COMPILER=#{Formula["open-mpi"].opt_bin}/mpicxx
+      -DCMAKE_CXX_FLAGS=-std=c++11 
       -DBUILD_SHARED_LIBS=ON
       -DTPL_ENABLE_PARMETISLIB=ON
-      -DTPL_PARMETIS_INCLUDE_DIRS="#{Formula["parmetis"].opt_include}"
-      -DTPL_PARMETIS_LIBRARIES="#{Formula["parmetis"].opt_lib}/libparmetis.dylib"
+      -DTPL_PARMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include}
+      -DTPL_PARMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.dylib
       -DTPL_ENABLE_INTERNAL_BLASLIB=OFF
-      -DTPL_BLAS_LIBRARIES="#{Formula["openblas"].opt_lib}/libopenblas.dylib"
+      -DTPL_BLAS_LIBRARIES=#{Formula["openblas"].opt_lib}/libopenblas.dylib
     ]
 
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
