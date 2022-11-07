5d7e8be476a0849daaa110b2d100367b1fe89db6
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-11-07 13:25:14 +0100

    petsc / slepc ventura

--- Formula/slepc.rb
+++ Formula/slepc.rb
@@ -43,7 +43,7 @@
 
   def install
     ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}"
-    system "./configure", "--with-arpack=1", "--with-arpack-dir=#{Formula["arpack"].opt_lib}"
+    system "./configure", "--with-arpack=1", "--with-arpack-dir=#{Formula["arpack"].opt_lib}",
                           "--prefix=#{prefix}",
                           "--with-clean=true"
     system "make"
