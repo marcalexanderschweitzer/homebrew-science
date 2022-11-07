18326b4fdfa1135605171c3fb9b473c24b9dd4ee
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-11-07 16:24:47 +0100

    superlu_dist ventura

--- Formula/superlu_dist.rb
+++ Formula/superlu_dist.rb
@@ -8,26 +8,24 @@
   depends_on "cmake"
   depends_on "metis"
   depends_on "openblas"
-  depends_on "gcc"
   depends_on "open-mpi"
   depends_on "marcalexanderschweitzer/science/parmetis"
 
   def install
     args = %W[
       -DCMAKE_C_COMPILER=#{Formula["open-mpi"].opt_bin}/mpicc
-      -DCMAKE_FC_COMPILER=#{Formula["open-mpi"].opt_bin}/mpif90
       -DCMAKE_CXX_COMPILER=#{Formula["open-mpi"].opt_bin}/mpicxx
       -DCMAKE_CXX_FLAGS=-std=c++11 
       -DBUILD_SHARED_LIBS=ON
       -DBUILD_STATIC_LIBS=OFF
       -DTPL_ENABLE_PARMETISLIB=ON
-      "-DTPL_PARMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include};#{Formula["metis"].opt_include}"
-      "-DTPL_PARMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.dylib;#{Formula["metis"].opt_lib}/libmetis.dylib"
       -DTPL_ENABLE_INTERNAL_BLASLIB=OFF
       -DTPL_BLAS_LIBRARIES=#{Formula["openblas"].opt_lib}/libopenblas.dylib
       -DXSDK_ENABLE_Fortran=OFF
       -Denable_openmp=OFF
     ]
+
+    args << "-DTPL_PARMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include};#{Formula["metis"].opt_include} -DTPL_PARMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.dylib;#{Formula["metis"].opt_lib}/libmetis.dylib"
 
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
