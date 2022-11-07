929e6b7368a3cff7c1c1b0e6359dfadefdde3ec5
Marc Alexander Schweitzer <marc.alexander.schweitzer@scai.fraunhofer.de>
2022-11-07 15:48:23 +0100

    superlu_dist ventura

--- Formula/superlu_dist.rb
+++ Formula/superlu_dist.rb
@@ -1,9 +1,9 @@
  class SuperluDist < Formula
   desc ""
   homepage "https://github.com/xiaoyeli/superlu_dist/"
-  version "5.3.0"
-  url "https://github.com/xiaoyeli/superlu/archive/refs/tags/v5.3.0.tar.gz"
-  sha256 "3e464afa77335de200aeb739074a11e96d9bef6d0b519950cfa6684c4be1f350"
+  version "8.1.0"
+  url "https://github.com/xiaoyeli/superlu_dist/archive/refs/tags/v8.1.0.tar.gz"
+  sha256 "9308844b99a7e762d5704934f7e9f79daf158b0bfc582994303c2e0b31518b34"
 
   depends_on "cmake"
   depends_on "metis"
@@ -16,11 +16,14 @@
 
     args = %W[
       -DCMAKE_C_COMPILER="#{Formula["open-mpi"].opt_bin}/mpicc"
+      -DCMAKE_FC_COMPILER="#{Formula["open-mpi"].opt_bin}/mpif90"
       -DCMAKE_CXX_COMPILER="#{Formula["open-mpi"].opt_bin}/mpicxx"
       -DCMAKE_CXX_FLAGS="-std=c++11" 
+      -DBUILD_SHARED_LIBS=ON
       -DTPL_ENABLE_PARMETISLIB=ON
       -DTPL_PARMETIS_INCLUDE_DIRS="#{Formula["parmetis"].opt_include}"
       -DTPL_PARMETIS_LIBRARIES="#{Formula["parmetis"].opt_lib}/libparmetis.dylib"
+      -DTPL_ENABLE_INTERNAL_BLASLIB=OFF
       -DTPL_BLAS_LIBRARIES="#{Formula["openblas"].opt_lib}/libopenblas.dylib"
     ]
 
